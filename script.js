c63efb4ef5e0c6792452ac53f0afaf9c192f32f1
WebDev47621 <webdev47621@gmail.com>
2021-01-05 16:18:07 -0800

    Updated files

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ VanillaTilt.init(document.querySelectorAll(".glass-tilt .row .card"), {
     speed: 400,
     glare: true
 });
+
+const stackSec = document.querySelector('.three-dim-stack');
+const stackRow = document.querySelector('.three-dim-stack .row');
+
+if (window.innerWidth <= 1568) {
+    stackRow.addEventListener('mouseover', expandSection);
+    stackRow.addEventListener('mouseleave', contractSection);
+} else {
+    stackRow.removeEventListener('mouseover', expandSection);
+    stackRow.removeEventListener('mouseleave', contractSection);
+}
+// stackRow.addEventListener('mouseover', expandSection);
+// stackRow.addEventListener('mouseleave', contractSection);
+
+function expandSection() {
+    stackSec.style.height = '1200px';
+}
+
+function contractSection() {
+    stackSec.style.height = '500px';
+}
